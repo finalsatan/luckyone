884dac1fc4efa7c879f3195ccb136e3a25ad590f
Murphy233 <zhangyhhust@gmail.com>
2025-01-26 11:35:38 +0800

    modify anvil account address and add makefile

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -12,7 +12,7 @@ abstract contract CodeConstants {
     int256 public MOCK_WEI_PER_UINT_LINK = 4e15;
 
     address public FOUNDRY_DEFAULT_SENDER =
-        0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
+        0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
 
     uint256 public constant ETH_SEPOLIA_CHAIN_ID = 11155111;
     uint256 public constant ETH_MAINNET_CHAIN_ID = 1;
